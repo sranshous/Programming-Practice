bbdcabfeebeec5535922ed93d9c68aa2605eb48f
Stephen Ranshous <sranshous@gmail.com>
2014-01-28 11:28:10 -0500

    Removed unnecessary list comprehensions by combining them

--- simple_arithmetic/sa.py
+++ simple_arithmetic/sa.py
@@ -4,7 +4,10 @@ writing your own calculator that allows you to type in a lot of numbers and sums
 them for you. Naturally, your calculator begins with a sum of 0.
 """
 
-import sys
-import re
+import sys, re
 
-print '\n'.join([str(ans) for ans in [sum(x) for x in [map(int, v) for v in [line.split() for line in re.findall('.*?0', ' '.join(re.split('\\s', ' '.join(sys.stdin.readlines()))))]]]])
+# Original
+#print '\n'.join([str(ans) for ans in [sum(x) for x in [map(int, v) for v in [line.split() for line in re.findall('.*?0', ' '.join(re.split('\\s', ' '.join(sys.stdin.readlines()))))]]]])
+
+# Better
+print '\n'.join([str(sum(map(int, line.split()))) for line in re.findall('.*?0', ' '.join(re.split('\\s', sys.stdin.read())))])
