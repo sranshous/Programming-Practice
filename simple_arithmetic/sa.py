db971e7e30c613cdd29264f3aff4c7b2eb1f9a7d
Stephen Ranshous <sranshous@gmail.com>
2014-01-27 19:49:31 -0500

    One line python solution

---
+++ simple_arithmetic/sa.py
@@ -0,0 +1,10 @@
+"""
+Tired of summing up numbers by hand for your night job as an accountant, you are
+writing your own calculator that allows you to type in a lot of numbers and sums
+them for you. Naturally, your calculator begins with a sum of 0.
+"""
+
+import sys
+import re
+
+print '\n'.join([str(ans) for ans in [sum(x) for x in [map(int, v) for v in [line.split() for line in re.findall('.*?0', ' '.join(re.split('\\s', ' '.join(sys.stdin.readlines()))))]]]])
