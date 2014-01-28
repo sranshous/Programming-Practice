42bddcf717f6bf691a3692e34bd983eee0107123
Stephen Ranshous <sranshous@gmail.com>
2014-01-28 11:35:39 -0500

    Reduced character count if imports are not included. Also switched from re.split to just the string split command

--- simple_arithmetic/sa.py
+++ simple_arithmetic/sa.py
@@ -4,10 +4,14 @@ writing your own calculator that allows you to type in a lot of numbers and sums
 them for you. Naturally, your calculator begins with a sum of 0.
 """
 
-import sys, re
+#import sys, re
+import sys as s, re as r
 
 # Original
 #print '\n'.join([str(ans) for ans in [sum(x) for x in [map(int, v) for v in [line.split() for line in re.findall('.*?0', ' '.join(re.split('\\s', ' '.join(sys.stdin.readlines()))))]]]])
 
 # Better
-print '\n'.join([str(sum(map(int, line.split()))) for line in re.findall('.*?0', ' '.join(re.split('\\s', sys.stdin.read())))])
+#print '\n'.join([str(sum(map(int, line.split()))) for line in re.findall('.*?0', ' '.join(re.split('\\s', sys.stdin.read())))])
+
+# Betterer
+print '\n'.join([str(sum(map(int, line.split()))) for line in r.findall('.*?0', ' '.join(s.stdin.read().split('\\s')))])
